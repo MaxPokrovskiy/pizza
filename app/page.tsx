6add5c686829796a1519c09d028ffa6b8cb187c2
Makc Pokrovsliy <makcumka9111@yandex.ru>
2024-11-28 12:21:45 +0300

    working to aside

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import {Categories, Container, SortPopup, Title, TopBar} from "@/components/shared";
+import {Categories, Container, SortPopup, Title, TopBar, Filters} from "@/components/shared";
+
 
 export default function Home() {
   return (
@@ -7,6 +8,22 @@ export default function Home() {
         <Title text="Все пиццы" size={"lg"} className="font-extrabold"/>
       </Container>
       <TopBar/>
+
+      <Container className="pb-14 mt-10">
+        <div className="flex gap-[60px]"></div>
+        {/*Фильтрация*/}
+        <div className="w-[250px]">
+          <Filters/>
+        </div>
+
+        {/*Список товаров*/}
+        <div className="flex-1">
+          <div className="flex flex-col gap-16">
+            {/*<ProductsGroupList title="Пиццы" items={[1,2,3,4,5]} />*/}
+            {/*<ProductsGroupList title="Комбо" items={[1,2,3,4,5]} />*/}
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
